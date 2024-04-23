4e6f9df9aac42650819b8cad0a133900a5a0b956
Anunay Argha <anunayargha@gmail.com>
2024-04-23 18:07:51 +0600

    project done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 function NavBar() {
 	const { user, logOut } = useAuth();
+	const [isOpen, setOpen] = useState(false);
+
 	return (
-		<nav className="flex justify-between items-center bg-[url(https://i.postimg.cc/jSg07Scm/15.jpg)] bg-cover bg-center lg:px-28">
+		<nav className="flex justify-between items-center bg-[url(https://i.postimg.cc/jSg07Scm/15.jpg)] bg-cover bg-center lg:px-28 px-4">
 			<Link to="/">
-				<figure className="text-white font-rancho text-3xl lg:text-6xl flex items-center justify-center lg:gap-4">
+				<figure className="text-white font-rancho text-3xl lg:text-6xl flex items-center justify-center lg:gap-4 gap-1">
 					<img
 						src="https://i.postimg.cc/HL41S99c/logo1.png"
 						alt="logo brand"
-						className="h-20 lg:h-24"
+						className="h-16 lg:h-24"
 					/>
 					<figcaption>Espresso Emporium</figcaption>
 				</figure>
 			</Link>
-			<div className="space-x-5">
+			<div className="space-x-5 hidden md:flex md:items-center">
 				{user ? (
 					<Button event={logOut}>Sign out</Button>
 				) : (
@@ -26,6 +30,9 @@ function NavBar() {
 					</>
 				)}
 			</div>
+			<button className="md:hidden text-white">
+				<Hamburger toggled={isOpen} toggle={setOpen} />
+			</button>
 		</nav>
 	);
 }
